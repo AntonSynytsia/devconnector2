22fe5e35e22ceb7ba99edd26135442123a0befce
AntonSynytsia <anton.synytsia@gmail.com>
2019-10-23 17:24:04 -0700

    Routing

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -40,6 +40,7 @@ export default function(state = initialState, action) {
     case CLEAR_PROFILE:
       return {
         ...state,
+        profile: null,
         repos: [],
         loading: false
       };
